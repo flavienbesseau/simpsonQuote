f9cf058111a7b2e77e2328b470b18d586615c657
FlavienBesseau <besseau.flavien@gmail.com>
2020-10-26 18:55:21 +0100

    call fetch

---
+++ script.js
@@ -0,0 +1,18 @@
+function fetchSimpsonQuoteJSON() {
+  const url = "https://simpsons-quotes-api.herokuapp.com/quotes";
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (quotes) {
+      const { quote, character, image, characterDirection } = quotes[0];
+
+      const citation = `<p><strong>${character}</strong></p>
+      <img src="${image}<"></img>
+      <p>${quote}</p>
+      <p>${characterDirection}</p>`;
+      document.querySelector("#simpson").innerHTML = citation;
+    });
+}
+
+fetchSimpsonQuoteJSON();
